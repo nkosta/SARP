35dc1e51fb91aeb5222db2a3d619396e5ae4d5b1
BostjanSkok <BostjanSkok2@gmail.com>
2015-01-12 00:36:51 +0100

    javadoc 3

--- app/src/main/java/com/example/sarp_mobile/PomozneMetode.java
+++ app/src/main/java/com/example/sarp_mobile/PomozneMetode.java
@@ -11,23 +11,21 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-public class PomozneMetode
-{
+public class PomozneMetode {
 
     /* Funkcija preveri ali v tabeli obstaja duplikat elementa. */
+
     /**
      * Checks if now two start times in table are the same
      *
      * @param tab Process start times
      * @return False if duplicate time found in table other wise true
      */
-    public static boolean hasEnakCasdospetja(int[] tab)
-    {
+    public static boolean hasEnakCasdospetja(int[] tab) {
         int val = tab[0];
 
-        for(int i=1;i<tab.length;i++)
-        {
-            if(val == tab[i])
+        for (int i = 1; i < tab.length; i++) {
+            if (val == tab[i])
                 return false;
 
             val = tab[i];
@@ -42,6 +40,7 @@ public class PomozneMetode
 	to service time.*/
 
     /* Metoda izračuna čakalne čase za posamezne procese algoritma PS.  */
+
     /**
      * Computes wait times for PS algorithm for the queue of processes passed in
      *
@@ -49,8 +48,7 @@ public class PomozneMetode
      * @param a           Results are feed directly into this AlgorithmsSwipeActivity instance
      * @throws Exception
      */
-    public static void izracunajCakalneCasePS(Priority_Proces ready_queue[], AlgorithmsSwipeActivity a) throws Exception
-    {
+    public static void izracunajCakalneCasePS(Priority_Proces ready_queue[], AlgorithmsSwipeActivity a) throws Exception {
         int tat = 0;
         int time = 0;
         double rntat = 0;
@@ -61,50 +59,52 @@ public class PomozneMetode
 
         TableLayout tabSim = (TableLayout) a.findViewById(R.id.simulation_table);
 
-        for(int i=0;i<ready_queue.length;i++)
-        {
+        for (int i = 0; i < ready_queue.length; i++) {
             TableRow row = new TableRow(a);
             TableRow.LayoutParams rowParams = new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             rowParams.gravity = Gravity.CENTER_HORIZONTAL;
             row.setLayoutParams(rowParams);
             tabSim.addView(row);
 
-            if(i == 0)
-            {
+            if (i == 0) {
                 time += ready_queue[0].get_trajanje_proc();
                 tat = (casCakanja + ready_queue[0].get_trajanje_proc());
-                rntat = (double)(tat/(double)ready_queue[0].get_trajanje_proc());
+                rntat = (double) (tat / (double) ready_queue[0].get_trajanje_proc());
                 vsotaTat += tat;
                 vsotaRnTat += rntat;
-            }
-
-            else
-            {
-                while(ready_queue[i].get_cas_dospetja_proc() > time)
+            } else {
+                while (ready_queue[i].get_cas_dospetja_proc() > time)
                     time++;
 
                 casCakanja = time - ready_queue[i].get_cas_dospetja_proc();
                 time += ready_queue[i].get_trajanje_proc();
                 vsotaCasovCakanja += casCakanja;
                 tat = (casCakanja + ready_queue[i].get_trajanje_proc());
-                rntat = (double)(tat/(double)ready_queue[i].get_trajanje_proc());
+                rntat = (double) (tat / (double) ready_queue[i].get_trajanje_proc());
                 vsotaTat += tat;
                 vsotaRnTat += rntat;
             }
 
-            String args[] = { ready_queue[i].get_ime_proc(), casCakanja + "",
-                    tat + "",  String.format("%.2f", rntat)};
+            String args[] = {ready_queue[i].get_ime_proc(), casCakanja + "",
+                    tat + "", String.format("%.2f", rntat)};
 
             tabSim.addView(fillSimTableRow(args, a));
         }
 
-        fillSimTabAverageValues(vsotaCasovCakanja/ready_queue.length, vsotaTat/ready_queue.length,
-                vsotaRnTat/ready_queue.length, a);
+        fillSimTabAverageValues(vsotaCasovCakanja / ready_queue.length, vsotaTat / ready_queue.length,
+                vsotaRnTat / ready_queue.length, a);
     }
 
     /* Metoda izračuna čakalne čase za posamezne procese algoritma FCFS.  */
-    public static void izracunajCakalneCaseFCFS(Proces ready_queue[], AlgorithmsSwipeActivity a) throws Exception
-    {
+
+    /**
+     * Calculates wait times for algorithm First-come, first-served (FCFS)
+     *
+     * @param ready_queue processes to process
+     * @param a           instance of  AlgorithmsSwipeActivity which will be updated with the results
+     * @throws Exception
+     */
+    public static void izracunajCakalneCaseFCFS(Proces ready_queue[], AlgorithmsSwipeActivity a) throws Exception {
         int tat;
         int time = 0;
         double rntat;
@@ -115,53 +115,49 @@ public class PomozneMetode
 
         TableLayout tabSim = (TableLayout) a.findViewById(R.id.simulation_table);
 
-        for(int i=0;i<ready_queue.length;i++)
-        {
-            if(i == 0)
-            {
+        for (int i = 0; i < ready_queue.length; i++) {
+            if (i == 0) {
                 time += ready_queue[0].get_trajanje_proc();
                 vsotaCasovCakanja += casCakanja;
                 tat = (casCakanja + ready_queue[0].get_trajanje_proc());
-                rntat = (double)(tat/(double)ready_queue[0].get_trajanje_proc());
+                rntat = (double) (tat / (double) ready_queue[0].get_trajanje_proc());
                 vsotaTat += tat;
                 vsotaRnTat += rntat;
 
-            }
-
-            else
-            {
-                while(ready_queue[i].get_cas_dospetja_proc() > time)
+            } else {
+                while (ready_queue[i].get_cas_dospetja_proc() > time)
                     time++;
 
                 casCakanja = time - ready_queue[i].get_cas_dospetja_proc();
                 time += ready_queue[i].get_trajanje_proc();
                 vsotaCasovCakanja += casCakanja;
                 tat = (casCakanja + ready_queue[i].get_trajanje_proc());
-                rntat = (tat/(double)ready_queue[i].get_trajanje_proc());
+                rntat = (tat / (double) ready_queue[i].get_trajanje_proc());
                 vsotaTat += tat;
                 vsotaRnTat += rntat;
 
             }
 
-            String args[] = { ready_queue[i].get_ime_proc(), casCakanja + "",
-                    tat + "",  String.format("%.2f", rntat)};
+            String args[] = {ready_queue[i].get_ime_proc(), casCakanja + "",
+                    tat + "", String.format("%.2f", rntat)};
 
             tabSim.addView(fillSimTableRow(args, a));
         }
 
-        fillSimTabAverageValues(vsotaCasovCakanja/ready_queue.length, vsotaTat/ready_queue.length,
-                vsotaRnTat/ready_queue.length, a);
+        fillSimTabAverageValues(vsotaCasovCakanja / ready_queue.length, vsotaTat / ready_queue.length,
+                vsotaRnTat / ready_queue.length, a);
     }
 
     /* Metoda izračuna čakalne čase za posamezne procese algoritma SJN.  */
+
     /**
      * Calculates wait times for Shortest job next SNJ algorithm for processes passed in
+     *
      * @param ready_queue queue of processes for calucaltion
-     * @param a  AlgorithmsSwipeActivity into which is updated with the results
+     * @param a           AlgorithmsSwipeActivity into which is updated with the results
      * @throws Exception
      */
-    public static void izracunajCakalneCaseSJN(Proces[] ready_queue, AlgorithmsSwipeActivity a) throws Exception
-    {
+    public static void izracunajCakalneCaseSJN(Proces[] ready_queue, AlgorithmsSwipeActivity a) throws Exception {
         int tat;
         int time = 0;
         double rntat;
@@ -172,59 +168,54 @@ public class PomozneMetode
 
         TableLayout tabSim = (TableLayout) a.findViewById(R.id.simulation_table);
 
-        for(int i=0; i<ready_queue.length; i++)
-        {
+        for (int i = 0; i < ready_queue.length; i++) {
             TableRow row = new TableRow(a);
             TableRow.LayoutParams rowParams = new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             rowParams.gravity = Gravity.CENTER_HORIZONTAL;
             row.setLayoutParams(rowParams);
             tabSim.addView(row);
 
-            if(i == 0)
-            {
+            if (i == 0) {
                 time += ready_queue[0].get_trajanje_proc();
                 casCakanja = ready_queue[0].get_cas_cakanja_proc();
                 vsotaCasovCakanja += casCakanja;
                 tat = (casCakanja + ready_queue[0].get_trajanje_proc());
-                rntat = (tat/(double)ready_queue[0].get_trajanje_proc());
+                rntat = (tat / (double) ready_queue[0].get_trajanje_proc());
                 vsotaTat += tat;
                 vsotaRnTat += rntat;
 
-            }
-
-            else
-            {
-                while(ready_queue[i].get_cas_dospetja_proc() > time)
+            } else {
+                while (ready_queue[i].get_cas_dospetja_proc() > time)
                     time++;
 
                 casCakanja = time - ready_queue[i].get_cas_dospetja_proc();
                 time += ready_queue[i].get_trajanje_proc();
                 vsotaCasovCakanja += casCakanja;
                 tat = (casCakanja + ready_queue[i].get_trajanje_proc());
-                rntat = (tat/(double)ready_queue[i].get_trajanje_proc());
+                rntat = (tat / (double) ready_queue[i].get_trajanje_proc());
                 vsotaTat += tat;
                 vsotaRnTat += rntat;
 
             }
 
-            String args[] = { ready_queue[i].get_ime_proc(), casCakanja + "",
-                    tat + "",  String.format("%.2f", rntat)};
+            String args[] = {ready_queue[i].get_ime_proc(), casCakanja + "",
+                    tat + "", String.format("%.2f", rntat)};
 
             tabSim.addView(fillSimTableRow(args, a));
 
         }
 
-        fillSimTabAverageValues(vsotaCasovCakanja/ready_queue.length, vsotaTat/ready_queue.length,
-                vsotaRnTat/ready_queue.length, a);
+        fillSimTabAverageValues(vsotaCasovCakanja / ready_queue.length, vsotaTat / ready_queue.length,
+                vsotaRnTat / ready_queue.length, a);
     }
+
     /**
-     *  Calculates the wait times for Round Robin algorithm
+     * Calculates the wait times for Round Robin algorithm
      *
      * @param ready_queue queue of processes to process
-     * @param a   AlgorithmsSwipeActivity instance which will be updated with the results
+     * @param a           AlgorithmsSwipeActivity instance which will be updated with the results
      */
-    public static void izracunajCakalneCaseRR(Proces[] ready_queue, AlgorithmsSwipeActivity a)
-    {
+    public static void izracunajCakalneCaseRR(Proces[] ready_queue, AlgorithmsSwipeActivity a) {
         int tat;
         double rntat;
         int casCakanja = 0;
@@ -234,23 +225,22 @@ public class PomozneMetode
 
         TableLayout tabSim = (TableLayout) a.findViewById(R.id.simulation_table);
 
-        for(int i=0; i<ready_queue.length; i++)
-        {
+        for (int i = 0; i < ready_queue.length; i++) {
             casCakanja = ready_queue[i].get_cas_cakanja_proc();
             vsotaCasovCakanja += casCakanja;
             tat = (casCakanja + ready_queue[i].get_trajanje_proc());
-            rntat = (tat/(double)ready_queue[i].get_trajanje_proc());
+            rntat = (tat / (double) ready_queue[i].get_trajanje_proc());
             vsotaTat += tat;
             vsotaRnTat += rntat;
 
-            String args[] = { ready_queue[i].get_ime_proc(), casCakanja + "",
-                              tat + "",  String.format("%.2f", rntat)};
+            String args[] = {ready_queue[i].get_ime_proc(), casCakanja + "",
+                    tat + "", String.format("%.2f", rntat)};
 
             tabSim.addView(fillSimTableRow(args, a));
         }
 
-        fillSimTabAverageValues(vsotaCasovCakanja/ready_queue.length, vsotaTat/ready_queue.length,
-                                vsotaRnTat/ready_queue.length, a);
+        fillSimTabAverageValues(vsotaCasovCakanja / ready_queue.length, vsotaTat / ready_queue.length,
+                vsotaRnTat / ready_queue.length, a);
     }
 
     public static View fillSimTableRow(String arguments[], AlgorithmsSwipeActivity a) {
@@ -261,7 +251,7 @@ public class PomozneMetode
 
         TextView textViewProcessName = new TextView(a);
         textViewProcessName.setTextColor(a.slovarBarv.get(arguments[0].charAt(0)));
-        textViewProcessName.setText(Html.fromHtml("<b>"+arguments[0]+"</b>"));
+        textViewProcessName.setText(Html.fromHtml("<b>" + arguments[0] + "</b>"));
         textViewProcessName.setTypeface(Typeface.SERIF);
         textViewProcessName.setGravity(Gravity.CENTER_HORIZONTAL);
 
--- app/src/main/java/com/example/sarp_mobile/Priority_Proces.java
+++ app/src/main/java/com/example/sarp_mobile/Priority_Proces.java
@@ -1,9 +1,9 @@
 package com.example.sarp_mobile;
+
 /**
  * Extends normal Process class and adds priority handling
  */
-public class Priority_Proces extends Proces
-{
+public class Priority_Proces extends Proces {
     /* Atribut, ki je značilen samo za določene algoritme */
     private int prioriteta_proc;
 
@@ -15,29 +15,33 @@ public class Priority_Proces extends Proces
 
     /* Privzeti konstruktor. V njem podedujemo atribute razreda Proces */
     /* in generiramo prioriteto procesa */
+
     /**
      * Use super to initialize Process super class
      * Generates randomly a priority value
      */
-    public Priority_Proces()
-    {
+    public Priority_Proces() {
         super();
         this.prioriteta_proc = gen_prioriteta();
     }
+
     /**
      * Empty constructor
+     *
      * @param n
      */
-    public Priority_Proces(Object n){}
+    public Priority_Proces(Object n) {
+    }
+
     /**
-     *  Instances instance with suplied parameters
-     * @param imeProcesa Process name
+     * Instances instance with suplied parameters
+     *
+     * @param imeProcesa         Process name
      * @param casDospetjaProcesa Process start time
      * @param casTrajanjaProcesa process duration
-     * @param prioritetaProcesa process priority
+     * @param prioritetaProcesa  process priority
      */
-    public Priority_Proces(String imeProcesa, int casDospetjaProcesa, int casTrajanjaProcesa, int prioritetaProcesa)
-    {
+    public Priority_Proces(String imeProcesa, int casDospetjaProcesa, int casTrajanjaProcesa, int prioritetaProcesa) {
         super.set_ime_proc(imeProcesa);
         super.set_cas_dospetja_proc(casDospetjaProcesa);
         super.set_trajanje_proc(casTrajanjaProcesa);
@@ -45,36 +49,36 @@ public class Priority_Proces extends Proces
     }
 
     /* get in set metodi za atribut prioriteta */
+
     /**
      * Setter for process priority
+     *
      * @param prioriteta_proc new value for priority
      */
-    protected void set_prioriteta_proc(int prioriteta_proc)
-    {
+    protected void set_prioriteta_proc(int prioriteta_proc) {
         this.prioriteta_proc = prioriteta_proc;
     }
+
     /**
      * Getter for Process priority
+     *
      * @return current Process priority
      */
-    protected int get_prioriteta_proc()
-    {
+    protected int get_prioriteta_proc() {
         return this.prioriteta_proc;
     }
 
     /* Generira naključno prioriteto procesa na intervalu 0 - 5 [0, min_p, max_p] */
-    private int gen_prioriteta()
-    {
-        if(Math.random() < 0.15)
+    private int gen_prioriteta() {
+        if (Math.random() < 0.15)
             return 0;
 
         else
-            return min_p + (int)(Math.random() * ((max_p - min_p) + 1));
+            return min_p + (int) (Math.random() * ((max_p - min_p) + 1));
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return super.toString() + "\t" + this.get_prioriteta_proc();
     }
 
